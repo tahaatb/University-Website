47e9f482176ecebbdbe2e695acf5439b2986e060
Taha Attibbi <tahaatb@gmail.com>
2022-04-23 17:25:53 +0100

    Moved js script into a seperate file

---
+++ script.js
@@ -0,0 +1,7 @@
+var navLinks = document.getElementById("navLinks");
+    function openMenu() {
+        navLinks.style.right = "0";
+    }
+    function hideMenu() {
+        navLinks.style.right = "-200px"
+    }
